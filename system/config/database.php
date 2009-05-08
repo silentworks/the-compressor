9768b16f6d62357dfba6d31837cc18b2a32c4018
Andrew <easylancer@gmail.com>
2009-05-08 13:59:48 +0100

    Updated to include database connection in core.

---
+++ system/config/database.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Database configuration file
+ */
+$db_group = "default";
+$db_driver = "mysql";
+ 
+$config['default']['host'] = 'localhost';
+$config['default']['username'] = '';
+$config['default']['password'] = '';
+$config['default']['database'] = '';
+$config['default']['prefix'] = '';
+$config['default']['pdo'] = true;
